6bd93996c0f10c0b3980b081b83339aa435dd505
Hend Aghil <hendmanga@gmail.com>
2022-01-25 02:07:26 +0200

    first push for UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import InputField from "./components/Input";
+import { Text, Flex, Box } from "@chakra-ui/react";
+import Counter from "./components/Counter";
+
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <Box display={"flex"} justifyContent={"center"} bg={"#f5f5f5"} h={"100vh"}>
+      <Flex w={{ md: "80%", sm: "95%" }} direction={"column"} rowGap={"10px"}>
+        <Text fontSize={18} color={"gray"} fontWeight={"bold"}>
+          Add your text here:
+        </Text>
+        <InputField />
+        <Counter wordsCount={"330"} charactersCount={1220} />
+      </Flex>
+    </Box>
+  );
 }
 
 export default App;
---
+++ src/components/Input.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Input } from "@chakra-ui/react";
+
+const InputField = () => {
+  return <Input bg={"white"} h="220px" placeholder=" " size="lg" />;
+};
+
+export default InputField;
