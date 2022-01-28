c1a102bb1cc708e701791cac144505d63e44fff4
Hend Aghil <hendmanga@gmail.com>
2022-01-28 17:25:07 +0200

    merge

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { Input } from "@chakra-ui/react";
+import { Textarea } from "@chakra-ui/react";
 
 const InputField = () => {
-  return <Input bg={"white"} h="220px" placeholder=" " size="lg" />;
+  return <Textarea bg={"white"} h="220px" placeholder=" " size="lg" />;
 };
 
 export default InputField;
